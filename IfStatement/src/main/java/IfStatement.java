de2e8acd413ce2112603280827536d8dee75aac4
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2022-12-20 21:25:15 +0000

    Saved progress on IfStatement 2022-12-20 21:25:15 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.fail;
 
 public class IfStatement {
     /**
@@ -17,6 +18,19 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+
+        if(bool == true)
+        {
+            return x;
+        }
+        else if(bool == false)
+        {
+            return y;
+        }
+
+
+
+
         return 0;
     }
 }

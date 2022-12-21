1b9b15adfd100b648d7e8573226a56ce0fd2839b
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2022-12-21 02:40:50 +0000

    Saved progress on Instantiation 2022-12-21 02:40:50 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Exception ex = new Exception();
+        return ex;
     }
 }

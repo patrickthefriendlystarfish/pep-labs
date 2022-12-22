4e7556a3d13e445348fc4b2dfa970621c1c441bb
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2022-12-22 20:34:58 +0000

    Saved progress on UncheckedException 2022-12-22 20:34:58 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class CauseUncheckedException {
     /**
      * This method must throw an unchecked exception.
@@ -14,57 +14,12 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-        long[] arr = new long[99999999];
-        long[] arr1 = new long[99999999];
-        long[] arr2 = new long[99999999];
-        long[] arr3 = new long[99999999];
-        long[] arr4 = new long[99999999];
-        long[] arr5 = new long[99999999];
-        long[] arr6 = new long[99999999];
-        long[] arr7 = new long[99999999];
-        long[] arr8 = new long[99999999];
-        long[] arr9 = new long[99999999];
-        long[] arr10 = new long[99999999];
-        long[] arr11 = new long[99999999];
-        long[] arr12 = new long[99999999];
-        long[] arr13 = new long[99999999];
-        long[] arr14 = new long[99999999];
-        long[] arr15 = new long[99999999];
-        long[] arr16 = new long[99999999];
-        long[] arr17 = new long[99999999];
-        long[] arr18 = new long[99999999];
-        long[] arr19 = new long[99999999];
-        long[] arr20 = new long[99999999];
-        long[] arr21 = new long[99999999];
-        long[] arr22 = new long[99999999];
-        long[] arr23 = new long[99999999];
-        long[] arr24 = new long[99999999];
-        long[] arr25 = new long[99999999];
-        long[] arr26 = new long[99999999];
-        long[] arr27 = new long[99999999];
-        long[] arr28 = new long[99999999];
-        long[] arr29 = new long[99999999];
-        long[] arr30 = new long[99999999];
-        long[] arr31 = new long[99999999];
-        long[] arr32 = new long[99999999];
-        long[] arr33 = new long[99999999];
-        long[] arr34 = new long[99999999];
-        long[] arr35 = new long[99999999];
-        long[] arr36 = new long[99999999];
-        long[] arr37 = new long[99999999];
-        long[] arr38 = new long[99999999];
-        long[] arr39 = new long[99999999];
-        long[] arr40 = new long[99999999];
-        long[] arr41 = new long[99999999];
-        long[] arr42 = new long[99999999];
-        long[] arr43 = new long[99999999];
-        long[] arr44 = new long[99999999];
-        long[] arr45 = new long[99999999];
-        long[] arr46 = new long[99999999];
-        long[] arr47 = new long[99999999];
-        long[] arr48 = new long[99999999];
-        long[] arr49 = new long[99999999];
-        long[] arr50 = new long[99999999];
+       String string = "Welcome to Windows 95!";
+       int num = 0;
+
+       num = Integer.parseInt(string);
+        
+
 
     }
 }

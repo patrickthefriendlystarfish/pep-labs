dcc84e5f2f201c6940c174abef4bfc57a2e5c467
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-07 16:04:23 +0000

    Saved progress on Queue 2023-01-07 16:04:23 +0000

--- Queue/src/main/java/QueueExercise.java
+++ Queue/src/main/java/QueueExercise.java
@@ -60,9 +60,11 @@ public class QueueExercise {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        String firstIndex = queue.element();
-        queue.poll();
-        return firstIndex;
+       
+         String head = queue.peek();
+         queue.remove();
+         return head;
+
     }
 
     /**

0a5f745cebe0c2c1e23b195485d9c3ac6f78af17
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-07 20:46:54 +0000

    Saved progress on BankAccount 2023-01-07 20:46:54 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -24,6 +24,8 @@ public class BankService {
      */
     public void deposit(double amount){
 
+        this.balance += amount;
+
     }
 
     /**
@@ -32,7 +34,7 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        this.balance -= amount;
     }
 
     /**
@@ -40,6 +42,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }

44ec9f23e223b9458bf5ac11863d44225f7b79d1
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-07 19:59:44 +0000

    Saved progress on GetMax 2023-01-07 19:59:44 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,31 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+
+        int len = arr.length;
+        int minIndex = 0; 
+        int temp = 0;
+        
+        
+        for(int i = 0; i < len - 1; i++)
+        {
+            minIndex = i;
+           
+            for(int j = i+1; j < len; j++)
+            {
+                
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+
+
+                 temp = arr[minIndex];
+                 arr[minIndex] = arr[i];
+                 arr[i] = temp;
+
+            }
+
+        }
     }
 }

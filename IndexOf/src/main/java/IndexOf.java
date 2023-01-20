bac27cd78d295526d414537a78df4a6672cbcf02
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-20 23:16:42 +0000

    Saved progress on IndexOf 2023-01-20 23:16:42 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -11,6 +11,6 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        return -1;
+        return 
     }
 }

50771733da9cf8f663f7db3b84b305101c9e2c15
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-25 21:46:24 +0000

    Saved progress on ForeignKey 2023-01-25 21:46:24 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post(id SERIAL PRIMARY KEY,post VARCHAR(255), user_fk int REFERENCES user(id));
+CREATE TABLE post(id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk int REFERENCES user(id));

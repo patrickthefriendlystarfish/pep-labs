9ad16889bad49a3cb82f7dfe90aa23b9b3afe578
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-01-26 01:49:18 +0000

    Saved progress on OuterJoin 2023-01-26 01:49:18 +0000

--- OuterJoin/problem2.sql
+++ OuterJoin/problem2.sql
@@ -1,1 +1,1 @@
-SELECT * FROM class RIGHT OUTER JOIN student ON class.class_title = student.class_title; 
+SELECT * FROM class RIGHT OUTER JOIN textbook ON class.class_title = textbook.class_title; 

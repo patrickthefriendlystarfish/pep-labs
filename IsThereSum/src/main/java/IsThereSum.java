ec6cccbec9760c4ca26064cfd96f22f2d4933a96
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-02-04 01:04:37 +0000

    Saved progress on IsThereSum 2023-02-04 01:04:37 +0000

--- IsThereSum/src/main/java/IsThereSum.java
+++ IsThereSum/src/main/java/IsThereSum.java
@@ -10,6 +10,17 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        for(int i = 1; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr.length; j++)
+            {
+                if(arr[j] + arr[i] == target)
+                {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }

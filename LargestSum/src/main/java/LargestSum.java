d0893795c45491e4c6dbccda0bbb3cd0d8b12af3
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-02-21 18:02:43 +0000

    Saved progress on LargestSum 2023-02-21 18:02:43 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -1,4 +1,6 @@
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,46 +14,41 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        List<Integer> sums = new ArrayList<>();
-        //for sorting the list
-        
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(i > 0)
-            {
-                for(int j = i; i > 0; j--)
-                {
-                    int currentNum = nums.get(j);
-                    int previousNum = nums.get(j - 1);
-                    System.out.println("i > 0, in nested for");
-                    int sum = currentNum + previousNum;
-                    System.out.println("Sum of current number plus previousNumber :" + sum);
-                    sums.add(sum);
-                }
-            }
 
-            for(int k = i; k < nums.size() - 1; k++)
+        List<Integer> sums = new ArrayList<>();
+       for(int i = 0; i < nums.size(); i++)
+       {
+          int currentNum = nums.get(i);
+
+          for(int count = i; count < nums.size(); count++)
+          {
+             int nextNum = nums.get(count);
+             int sum = currentNum + nextNum;
+             sums.add(sum);
+          }
+
+          for(int count = i - 1; count > 0; count--)
+          {
+            int prevNum = nums.get(count);
+            int sum = currentNum + prevNum;
+            sums.add(sum);
+          }
+       }
+
+       for(int i = 0; i < sums.size(); i++)
+       {
+            for(int j = 1; j < (sums.size() - i); j++)
             {
-                int currentNum = nums.get(k);
-                int nextNum = nums.get(k + 1);
-                System.out.println("In other nested for loop");
-                int sum = currentNum + nextNum;
-                System.out.println("Sum of currentNumber plus nextNum: " + sum);
-                sums.add(sum);
-            }
-        }
-       
-        for (int i = 0; i < sums.size() - 1; i++)
-        {
-            for (int j = 0; j < sums.size() - i - 1; j++)
-                if (sums.get(j) > sums.get(j + 1)) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = sums.get(j);
-                    sums.set(j, j + 1);                 
-                    sums.set(j+1, temp);
+                if(sums.get(j - 1) > sums.get(j))
+                {
+                    int temp = sums.get(j - 1);
+                    sums.set(j - 1 , j);
+                    sums.set(j, temp);
                 }
-        }
-        return sums.get(sums.size() - 1);
+            }
+       }
+
+       return sums.get(sums.size() - 1);
 }
     
 }

ee49f51da037f7695d4cb80e452a104abdf0426d
patrickthefriendlystarfish <patrickpowers221@gmail.com>
2023-02-21 17:08:15 +0000

    Saved progress on LargestSum 2023-02-21 17:08:15 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -36,7 +36,7 @@ public class LargestSum {
         }
 
         //sorting the List
-        for(int i = 0; i < sums.size() - 1; i++)
+        for(int i = sums.size() - 1; i > 0; i--)
         {
             for(int j = 0; j < i; j++)
             {
